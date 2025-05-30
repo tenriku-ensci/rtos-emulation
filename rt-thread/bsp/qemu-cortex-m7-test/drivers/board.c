17bd3b515a32aa3e450bda33f6bfe9cf6613403a
tenriku-t <213159611+tenriku-t@users.noreply.github.com>
2025-05-30 12:36:16 +0900

    Rename MPS2 BSP and add startup code

---
+++ rt-thread/bsp/qemu-cortex-m7-test/drivers/board.c
@@ -0,0 +1,71 @@
+#include <rthw.h>
+#include <rtthread.h>
+#include "board.h"
+
+#define SYST_CSR   (*(volatile uint32_t*)0xE000E010)
+#define SYST_RVR   (*(volatile uint32_t*)0xE000E014)
+#define SYST_CVR   (*(volatile uint32_t*)0xE000E018)
+
+#define UART0_BASE    0x40004000
+#define UART_DATA     (*(volatile uint32_t*)(UART0_BASE + 0x00))
+#define UART_STATE    (*(volatile uint32_t*)(UART0_BASE + 0x04))
+#define UART_CTRL     (*(volatile uint32_t*)(UART0_BASE + 0x08))
+#define UART_BAUDDIV  (*(volatile uint32_t*)(UART0_BASE + 0x10))
+#define UART_STATE_TXFULL 0x01
+
+#ifndef SYSTEM_CORE_CLOCK
+#define SYSTEM_CORE_CLOCK 100000000
+#endif
+static uint32_t SystemCoreClock = SYSTEM_CORE_CLOCK;
+
+void SystemInit(void)
+{
+    /* nothing to configure */
+}
+
+static void uart_init(void)
+{
+    /* 115200 baud with 50 MHz clock -> divider 27 (approx) */
+    UART_BAUDDIV = 27;
+    UART_CTRL = 0x03; /* enable TX and RX */
+}
+
+void rt_hw_console_output(const char *str)
+{
+    while (*str)
+    {
+        if (*str == '\n')
+        {
+            while (UART_STATE & UART_STATE_TXFULL);
+            UART_DATA = '\r';
+        }
+        while (UART_STATE & UART_STATE_TXFULL);
+        UART_DATA = *str++;
+    }
+}
+
+void SysTick_Handler(void)
+{
+    rt_interrupt_enter();
+    rt_tick_increase();
+    rt_interrupt_leave();
+}
+
+void rt_hw_board_init(void)
+{
+#ifdef RT_USING_HEAP
+    rt_system_heap_init(HEAP_BEGIN, HEAP_END);
+#endif
+
+    SYST_RVR = SystemCoreClock / RT_TICK_PER_SECOND - 1;
+    SYST_CVR = 0;
+    SYST_CSR = 7;
+
+#ifdef RT_USING_CONSOLE
+    uart_init();
+    rt_console_set_device(RT_CONSOLE_DEVICE_NAME);
+#endif
+#ifdef RT_USING_COMPONENTS_INIT
+    rt_components_board_init();
+#endif
+}
---
+++ rt-thread/bsp/qemu-cortex-m7-test/rtconfig.h
@@ -0,0 +1,33 @@
+#ifndef RT_CONFIG_H__
+#define RT_CONFIG_H__
+
+/* RT-Thread Kernel */
+#define RT_CPUS_NR 1
+#define RT_NAME_MAX 16
+#define RT_ALIGN_SIZE 4
+#define RT_THREAD_PRIORITY_32
+#define RT_THREAD_PRIORITY_MAX 32
+#define RT_TICK_PER_SECOND 100
+#define RT_USING_OVERFLOW_CHECK
+#define RT_USING_HEAP
+#define RT_USING_COMPONENTS_INIT
+#define IDLE_THREAD_STACK_SIZE 512
+#define RT_CONSOLEBUF_SIZE 256
+#define RT_BACKTRACE_LEVEL_MAX_NR 32
+#define RT_USING_USER_MAIN
+#define RT_MAIN_THREAD_STACK_SIZE 2048
+
+/* Device drivers */
+#define RT_USING_DEVICE
+#define RT_USING_CONSOLE
+#define RT_CONSOLE_DEVICE_NAME "uart0"
+
+/* Shell */
+
+/* Drivers */
+#define BSP_USING_UART
+#define BSP_USING_UART0
+
+#define SOC_MPS2_AN500
+
+#endif
